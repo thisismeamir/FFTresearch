7f6f4311f82cdacdc8197dce3fc9339c1cb59c8d
thisismeamir <thisismeamir@outlook.com>
2022-03-21 14:40:07 +0330

    Functions.py completed

---
+++ FourierSeries/Fourier.py
@@ -0,0 +1,11 @@
+from Functions import Func2D as func
+import sympy as sp
+import numpy as np
+import matplotlib as mp
+import pandas as pd
+
+class FourierSeries():
+    def __init__(self):
+        assert self.func.ready == True
+        pass
+    def SymbolicSeries(self):
